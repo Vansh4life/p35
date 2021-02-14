7f8d06a1290f77b16274c5c5dbe1f455b7f81269
Vansh4life <77148767+Vansh4life@users.noreply.github.com>
2021-02-14 20:38:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var balloon;
+
+function preload(){
+  backgroundImg = loadImage("pro-C35 images/Hot Air Ballon-01.png");
+
+  balloonImg = loadImage("pro-C35 images/Hot Air Balloon-02.png");
+}
+
+function setup() {
+  createCanvas(500,500);
+
+  database = firebase.database();
+
+  balloon = createSprite(400, 200, 50, 50);
+  balloon.addImage(balloonImg);
+}
+
+function draw() {
+  background(backgroundImg);  
+
+  if(keyDown(LEFT_ARROW)){
+    balloon.x = balloon.x - 10;
+}
+else if(keyDown(RIGHT_ARROW)){
+  balloon.x = balloon.x + 10;
+}
+else if(keyDown(UP_ARROW)){
+  balloon.y = balloon.y - 10;
+}
+else if(keyDown(DOWN_ARROW)){
+  balloon.y = balloon.y + 10;
+}
+
+  drawSprites();
+}
